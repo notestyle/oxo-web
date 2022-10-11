5d83600def575c740040d1d95a47b9f222e5bf36
ariunjargal ariunbold <ariunjargal.nest@gmail.com>
2022-10-11 11:13:24 +0800

    turshilt

---
+++ a.js
@@ -0,0 +1,16 @@
+export default function L3() {
+    return <div className="w-screen h-screen flex flex-col gap-4">
+        <div className=" bg-yellow-300 h-1/4 w-full"> </div>
+        <div className=" h-1/4 w-full flex gap-2"> 
+            <div className=" bg-yellow-300 h-full w-1/3"> </div>
+            <div className=" bg-yellow-300 h-full w-1/3"> </div>
+            <div className=" bg-yellow-300 h-full w-1/3"> </div>
+        </div>
+        <div className=" h-1/4 w-full flex gap-2"> 
+            <div className=" bg-yellow-300 h-full w-1/3"> </div>
+            <div className=" bg-yellow-300 h-full w-1/3"> </div>
+            <div className=" bg-yellow-300 h-full w-1/3"> </div>
+        </div>
+        <div className=" bg-yellow-300  h-1/4 w-full"> </div>
+    </div> 
+}
